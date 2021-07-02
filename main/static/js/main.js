f2001b2bcae8a675ff9a88c6a08a20fba1bd1830
Umuntu-solutions <86285116+Umuntu-solutions@users.noreply.github.com>
2021-07-02 23:23:58 +0530

    Update main.js

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -45,18 +45,17 @@ Webcam.set({
     jpeg_quality: 90
 });
 Webcam.attach('#imageCapture');
-function take_snapshot() {
+ 
 			// take snapshot and get image data
-    Webcam.snap( function(data_uri) {
+    Webcam.snap( (data_uri) {
 				// display results in page
         document.getElementById('results').innerHTML = 
 					'<h2></h2>' + 
 					'<img src="'+data_uri+'"/>';
 			} );
-		}
 	
  Webcam.reset();
-Webcam.attach('#imageCapture');
+Webcam.attach('#mageCapture');
  
 playbtn.addEventListener("click", playPause);
 nextbtn.addEventListener("click", () => { nextSong(mood) });
